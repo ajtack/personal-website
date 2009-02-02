2c045dffc306a2d2f4b9bb09ba2e6c4cfc750b7b
Andres Tack <tack@lhotse.cs.wisc.edu>
2009-02-01 23:09:38 -0600

    Fixed automatic titles to work with relative url roots. Also fixed an authorship association bug which prevented creation of authorships from the console.

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,7 +1,7 @@
 class Authorship < ActiveRecord::Base
 	belongs_to :author
 	belongs_to :publication
-	acts_as_list :scope => 'publication'
+	acts_as_list :scope => :publication	
 	
 	validates_presence_of :author, :publication
 end
