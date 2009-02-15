30526ab20b9c99778fd37a7b021dca988da728e1
Andres Jaan Tack <tack@Andresi-Arvuti.(none)>
2009-02-14 20:08:24 -0600

    All URLs are now properly localized and have the default as English.
    - Tests are a little ghetto; I don't completely understand the routing assertions.
    - Selections are tested; in either language, the current page is selected on top.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	LocalePrefix = /^\/([a-zA-Z]{2})(?=\/|$)/
+	
+	def locale_independent_path
+		request.path.gsub(LocalePrefix, '')
+	end
+
 	def public_links(path = nil)
 		link_labels = {
 				publications_path => t(:Publications),
@@ -13,18 +19,18 @@ module ApplicationHelper
 		end
 	end
 	
-	def page_titles(path = nil)
+	def page_title_for(path = nil)
 		titles = {
-				publications_path => t(:Publications),
-				about_path => t(:About_me),
-				index_path => t(:Home),
-				root_path => t(:Home)
+				publications_path.gsub(LocalePrefix, '') => t(:Publications),
+				about_path.gsub(LocalePrefix, '') => t(:About_me),
+				index_path.gsub(LocalePrefix, '') => t(:Home),
+				root_path.gsub(LocalePrefix, '') => t(:Home)
 			}
 		
 		if path.nil?
 			titles
 		else
-			titles[path]
+			titles[path.gsub(LocalePrefix, '')]
 		end
 	end
 end
