abd10d2161d501d8deb886085fada35266886382
Andres Jaan Tack <ajtack@gmail.com>
2009-03-15 04:07:14 -0500

    Applied public/private protection to ApplicationHelper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,34 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-	RelativeRootPrefix = /^#{ActionController::Base.relative_url_root}/
-	LocalePrefix = /^\/([a-zA-Z]{2})(?=\/|$)/
+	private
+		RelativeRootPrefix = /^#{ActionController::Base.relative_url_root}/
+		LocalePrefix = /^\/([a-zA-Z]{2})(?=\/|$)/
 	
-	def locale_independentize(path = request.path)
-		newpath = path
-		newpath.gsub!(RelativeRootPrefix, '')  # Only happens when testing.
-		newpath.gsub!(LocalePrefix, '')
-		newpath
-	end
+		def locale_independentize(path = request.path)
+			newpath = path.gsub(RelativeRootPrefix, '')  # Only happens when testing.
+			newpath.gsub!(LocalePrefix, '')
+			newpath
+		end
 
-	def public_links
-		link_labels = {
-				locale_independentize(publications_path) => {:title => t('page_titles.publications'), :path => publications_path},
-				locale_independentize(about_path) => {:title => t('page_titles.about_me'), :path => about_path},
-				locale_independentize(cv_path) => {:title => t('page_titles.cv'), :path => cv_path}
-			}
+	public
+		def public_links
+			link_labels = {
+					locale_independentize(publications_path) => {:title => t('page_titles.publications'), :path => publications_path},
+					locale_independentize(about_path) => {:title => t('page_titles.about_me'), :path => about_path},
+					locale_independentize(cv_path) => {:title => t('page_titles.cv'), :path => cv_path}
+				}
 			
-		link_labels
-	end
+			link_labels
+		end
 	
-	def page_title_for(path = nil)
-		titles = {
-				locale_independentize(about_path) => t('page_titles.about_me'),
-				locale_independentize(index_path) => t('page_titles.home'),
-				locale_independentize(root_path) => t('page_titles.home'),
-				locale_independentize(publications_path) => t('page_titles.publications'),
-				locale_independentize(cv_path) => t('page_titles.cv')
-			}
+		def page_title_for(path = nil)
+			titles = {
+					locale_independentize(about_path) => t('page_titles.about_me'),
+					locale_independentize(index_path) => t('page_titles.home'),
+					locale_independentize(root_path) => t('page_titles.home'),
+					locale_independentize(publications_path) => t('page_titles.publications'),
+					locale_independentize(cv_path) => t('page_titles.cv')
+				}
 			
-		titles[locale_independentize(path)]
-	end
+			titles[locale_independentize(path)]
+		end
 end
