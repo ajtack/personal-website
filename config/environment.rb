03d59cdc7b3958a0714657f297b5bea33ce45ceb
Andres Jaan Tack <ajtack@gmail.com>
2009-03-15 00:55:24 -0500

    Moved the WebRat gem to test environments only.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -17,6 +17,4 @@ Rails::Initializer.run do |config|
 		:session_key => '_website_session',
 		:secret      => '5ea7f317ca16ad18f0f0f2e9b6cd6f185e4785ad2acc32ebc6661a3a68b017a04159487f7a060332765ae5e393b55085d8cfdce5a0f8b78330b6da37d6f57399'
 	}
-	
-	config.gem 'webrat'
 end
