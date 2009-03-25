c23dbf5ce709ed5abe3eb2e91518bf1a8a4cbd70
Andres Jaan Tack <ajtack@gmail.com>
2009-03-24 21:19:29 -0500

    Included the restful-authentication module for treatment of login.
    - Updated stories to not describe anonymous user signups.
    - Added a basic story for blog management
    - Generated RSpec stubs for future testing and test conversion.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,10 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+	include AuthenticatedSystem
+	
   helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'efb3382558c540a1077236e6c50aed26'
-  
   before_filter :choose_locale
 
 	def choose_locale
