d7ad9c7ed05b5840ce6e2ed902f584578257532d
Andres Tack <tack@lhotse.cs.wisc.edu>
2009-06-03 16:28:25 -0500

    Added a reference to the rails root, making the CV pdf download correctly on my screwy pages.cs setup.

--- app/controllers/cvs_controller.rb
+++ app/controllers/cvs_controller.rb
@@ -2,7 +2,7 @@ class CvsController < ApplicationController
 	def show
 		respond_to do |format|
 			format.html # show.html.erb
-			format.pdf	{ send_file 'app/views/cvs/show.pdf' }
+			format.pdf	{ send_file Rails.root + 'app/views/cvs/show.pdf' }
 		end
 	end
 end
