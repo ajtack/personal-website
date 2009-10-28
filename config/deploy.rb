8f5e4f30dd4963030133700631a072dc8ea3d635
Andres Jaan Tack <ajtack@gmail.com>
2009-10-27 21:16:27 -0500

    Completed the deployment script correctly.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,18 @@
 set :application, 'cshome'
-set :user 'minusuu'
-set :domain 'minusuu.com'
-set :deploy_to, '/home/minusuu/rails/cshome'
-set :server_hostname, 'cshome.minusuu.com'
+set :user, 'minusuu'
+set :domain, 'cshome.minusuu.com'
+set :deploy_to, '/home/minusuu/sites/cshome'
+set :server_hostname, 'minusuu.com'
 
 set :scm, 'git'
-set :repository,  'git://github.com/ajtack/personal-website.git'
+set :repository, 'git://github.com/ajtack/personal-website.git'
 
 ssh_options[:forward_agent] = true
 set :branch, 'master'
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
+set :git_update_submodules, 1
 set :use_sudo, false
 set :deploy_to, "/home/#{user}/sites/#{application}"
 
@@ -19,12 +20,23 @@ role :app, server_hostname
 role :web, server_hostname
 role :db,  server_hostname, :primary => true
 
-set :repository,  "#{user}@#{domain}:/home/#{user}/git/#{application}"
-set :scm_username, user
-set :runner, user
-set :use_sudo, false
-set :branch, "master"
-set :deploy_via, :checkout
-set :git_shallow_clone, 1
-set :deploy_to, "/home/#{user}/apps/#{application}"
-default_run_options[:pty] = true
+namespace :passenger do
+  desc "Restart Application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart do
+    find_and_execute_task('passenger:restart')
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with Passenger."
+    task t, :roles => :app do ; end
+  end
+end
+
+after 'deploy', 'deploy:cleanup'
